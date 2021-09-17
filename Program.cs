b99f404bd340bcb00e59cd8f7f08b29c7478b212
murillojoaquim <murillojalves@hotmail.com>
2021-09-17 19:11:49 -0300

    Calculo Juros e Montante

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Capital: ");
+            string Capital = Console.ReadLine();
+
+            Console.WriteLine("Digite a Taxa de juros: ");
+            string Taxa_Juros = Console.ReadLine();
+            double Taxa_juros = Convert.ToDouble(Taxa_Juros) / 100;
+
+            Console.WriteLine("Digite a quantidade de meses");
+            string Meses = Console.ReadLine();
+            
+
+            double Juros = Convert.ToDouble(Capital) * Taxa_juros * Convert.ToDouble(Meses);
+            double Montante = Convert.ToDouble(Capital) + Juros;
+            
+            Console.WriteLine($"Juros (R$) = {Juros}");
+            Console.WriteLine($"Montante (R$)= {Montante}");
+
+
+        }
+    }
+}
